d053b0f0017768e0a9038be3836aef813c90de03
odyssic <stevenbuddenjr@gmail.com>
2020-05-05 15:58:09 -0400

    working hands

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-const secondHand = document.querySelector(".second-hand");
-
 function setDate() {
     const now = new Date();
+    const secondHand = document.querySelector(".second-hand");
     const seconds = now.getSeconds();
     const secondsDegrees = (seconds / 60) * 360 + 90;
+
+    const minuteHand = document.querySelector(".minute-hand");
+    const minutes = now.getMinutes();
+    const minutesDegrees = (minutes / 60) * 360 + 90;
+
+    const hourHand = document.querySelector(".hour-hand");
+    const hours = now.getHours();
+    const hoursDegrees = (hours / 60) * 360 + 90;
+
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
