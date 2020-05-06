3367c19a46e9bf815a18c4793886990f9535ee49
odyssic <stevenbuddenjr@gmail.com>
2020-05-05 21:08:45 -0400

    working now

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function setDate() {
 
     const minuteHand = document.querySelector(".minute-hand");
     const minutes = now.getMinutes();
-    const minutesDegrees = (minutes / 60) * 360 + 90;
+    const minutesDegrees = (minutes / 60) * 360;
 
     const hourHand = document.querySelector(".hour-hand");
     const hours = now.getHours();
-    const hoursDegrees = (hours / 60) * 360 + 90;
+    const hoursDegrees = (hours / 60) * 360;
 
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
